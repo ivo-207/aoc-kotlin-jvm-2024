ddfba91e1a0078146a7f6490b52b2844672fe79a
Łukasz Angulski <lukasz.angulski@gmail.com>
2024-12-01 11:54:57 +0100

    Day 1

--- day1/src/main/kotlin/Day1.kt
+++ day1/src/main/kotlin/Day1.kt
@@ -1,19 +1,50 @@
+import kotlin.math.abs
+import kotlin.text.Charsets.US_ASCII
+
+private val numbers = readInput("Day1.input", ::toIntArray)
+
+private val leftList = IntArray(numbers.size / 2) { numbers[it * 2] }
+    .sortedArray()
+
+private val rightList = IntArray(numbers.size / 2) { numbers[it * 2 + 1] }
+    .sortedArray()
+
 fun main() {
     println(part1())
     println(part2())
 }
 
 fun part1(): Any {
-    return input
-
+    return (0..<numbers.size / 2)
+        .sumOf { abs(leftList[it] - rightList[it]) }
 }
 
 fun part2(): Any {
-    return input.reversed()
+    return (0..<numbers.size / 2)
+        .sumOf {
+            val firstValue = leftList[it]
+            val score = rightList.count { it == firstValue }
+            firstValue * score
+        }
 }
 
-private val input: String by lazy {readInput()}
 
-private fun readInput(): String {
-    return object {}.javaClass.getResource("Day1.input").readText()
+private fun <T> readInput(name: String, transform: (Sequence<String>) -> T): T {
+    return object {}.javaClass.getResourceAsStream(name)
+        .use { it ->
+            it?.bufferedReader(US_ASCII)
+                .use {
+                    val sequence = it?.lineSequence() ?: emptySequence()
+                    transform(sequence)
+                }
+        }
+}
+
+private fun toIntArray(lines: Sequence<String>): IntArray {
+    val regex = "\\s+".toRegex()
+    return lines
+        .flatMap { it.splitToSequence(regex) }
+        .map { it.toInt() }
+        .toList()
+        .toIntArray()
 }
