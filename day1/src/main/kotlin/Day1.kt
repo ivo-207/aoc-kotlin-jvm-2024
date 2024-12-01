8ce877d449ad741e3d414a80fe8186ffe4f4d22c
Łukasz Angulski <lukasz.angulski@gmail.com>
2024-12-01 19:22:09 +0100

    Day 1 - optymalizacja

--- day1/src/main/kotlin/Day1.kt
+++ day1/src/main/kotlin/Day1.kt
@@ -1,4 +1,4 @@
-import kotlin.math.abs
+import kotlin.math.absoluteValue
 import kotlin.text.Charsets.US_ASCII
 
 private val numbers = readInput("Day1.input", ::toIntArray)
@@ -15,35 +15,51 @@ fun main() {
 }
 
 fun part1(): Any {
-    return (0..<numbers.size / 2)
-        .sumOf { abs(leftList[it] - rightList[it]) }
+    var distance = 0
+    val limit = numbers.size / 2
+    var i = 0
+    while (i < limit) {
+        distance += (leftList[i] - rightList[i]).absoluteValue
+        i += 1
+    }
+    return distance
 }
 
 fun part2(): Any {
-    return (0..<numbers.size / 2)
-        .sumOf {
-            val firstValue = leftList[it]
-            val score = rightList.count { it == firstValue }
-            firstValue * score
+    var leftIndex = 0
+    var rightIndex = 0
+    var score = 0
+    while (leftIndex < leftList.size && rightIndex < rightList.size) {
+        val left = leftList[leftIndex]
+        var leftCount = 1
+        while (++leftIndex < leftList.size && left == leftList[leftIndex]) {
+            leftCount += 1
         }
+        var rightCount = 0
+        while (rightIndex < rightList.size && rightList[rightIndex] <= left) {
+            val right = rightList[rightIndex++]
+            rightCount += if (left == right) 1 else 0
+        }
+        score += left * leftCount * rightCount
+    }
+    return score
 }
 
-
 private fun <T> readInput(name: String, transform: (Sequence<String>) -> T): T {
-    return object {}.javaClass.getResourceAsStream(name)
-        .use { it ->
-            it?.bufferedReader(US_ASCII)
-                .use {
-                    val sequence = it?.lineSequence() ?: emptySequence()
-                    transform(sequence)
-                }
+    val bufferSize = 1024 * 1024
+    return object {}.javaClass.getResourceAsStream(name).use { inputStream ->
+        inputStream?.reader(US_ASCII).use { reader ->
+            reader?.buffered(bufferSize).use {
+                val lines = it?.lineSequence() ?: emptySequence()
+                transform(lines)
+            }
         }
+    }
 }
 
 private fun toIntArray(lines: Sequence<String>): IntArray {
-    val regex = "\\s+".toRegex()
     return lines
-        .flatMap { it.splitToSequence(regex) }
+        .flatMap { it.splitToSequence("   ") }
         .map { it.toInt() }
         .toList()
         .toIntArray()
