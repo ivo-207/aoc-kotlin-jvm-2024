7566b96093a3a7c1a17141f460345b766c55a14a
Łukasz Angulski <lukasz.angulski@gmail.com>
2024-12-08 11:17:04 +0100

    Day 5

--- day5/src/main/kotlin/Day5.kt
+++ day5/src/main/kotlin/Day5.kt
@@ -1,19 +1,53 @@
+private val pageOrderingRuleRegex = "^(\\d+)\\|(\\d+)$".toRegex()
+private val updateRegex = "^(\\d+)(?:,(\\d+))+$".toRegex()
+
+private val comparator = readInput()
+    .mapNotNull { pageOrderingRuleRegex.find(it) }
+    .map {
+        val predecessor = it.groups[1]!!.value.toInt()
+        val successor = it.groups[2]!!.value.toInt()
+        Comparator<Int> { a, b ->
+            if (a == predecessor && b == successor) return@Comparator -1
+            if (a == successor && b == predecessor) return@Comparator 1
+            return@Comparator 0
+        }
+    }
+    .reduce(Comparator<Int>::thenComparing)
+
+private val updatesAndCorrectlyOrderedUpdates = readInput()
+    .mapNotNull { updateRegex.find(it) }
+    .map { it.value.split(",").map { it.toInt() }.toIntArray() }
+    .map { arrayOf(it, it.correctlyOrdered()) }
+    .toTypedArray()
+
 fun main() {
     println(part1())
     println(part2())
 }
 
 fun part1(): Any {
-    return input
-
+    return updatesAndCorrectlyOrderedUpdates
+        .filter { it[0].contentEquals(it[1]) }
+        .sumOf { it[0].middle() }
 }
 
 fun part2(): Any {
-    return input.reversed()
+    return updatesAndCorrectlyOrderedUpdates
+        .filterNot { it[0].contentEquals(it[1]) }
+        .sumOf { it[1].middle() }
 }
 
-private val input: String by lazy {readInput()}
+private fun readInput(): List<String> {
+    return object {}.javaClass.getResource("Day5.input")
+        ?.readText()
+        ?.split("\n")
+        ?: emptyList()
+}
 
-private fun readInput(): String {
-    return object {}.javaClass.getResource("Day5.input").readText()
+private fun IntArray.middle(): Int {
+    return this[size / 2]
+}
+
+private fun IntArray.correctlyOrdered(): IntArray {
+    return sortedWith(comparator).toIntArray()
 }
