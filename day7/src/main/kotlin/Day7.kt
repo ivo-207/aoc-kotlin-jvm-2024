302cc9a9313bda1e51e906f38b27bf45b2e289a6
Łukasz Angulski <lukasz.angulski@gmail.com>
2024-12-09 01:14:12 +0100

    Day 7

--- day7/src/main/kotlin/Day7.kt
+++ day7/src/main/kotlin/Day7.kt
@@ -1,19 +1,52 @@
+private val equations = readInput()
+    .map {
+        val testValueAndNumbers = it.split(": ")
+        val testValue = testValueAndNumbers[0].toLong()
+        val numbers = testValueAndNumbers[1].split(" ").map { it.toLong() }.toLongArray()
+        Equation(testValue, numbers)
+    }
+    .toTypedArray()
+
 fun main() {
     println(part1())
     println(part2())
 }
 
 fun part1(): Any {
-    return input
-
+    val operators = arrayOf(Operator.ADD, Operator.MULTIPLY)
+    return getTotalCalibrationResult(*operators)
 }
 
 fun part2(): Any {
-    return input.reversed()
+    val operators = arrayOf(Operator.ADD, Operator.MULTIPLY, Operator.CONCATENATION)
+    return getTotalCalibrationResult(*operators)
 }
 
-private val input: String by lazy {readInput()}
+private fun readInput(): List<String> {
+    return object {}.javaClass.getResource("Day7.input")
+        ?.readText()
+        ?.split("\n")
+        ?.filter { it.isNotEmpty() }
+        ?: emptyList()
+}
 
-private fun readInput(): String {
-    return object {}.javaClass.getResource("Day7.input").readText()
+private fun getTotalCalibrationResult(vararg operators: Operator): Long {
+    val operatorsGroupCache = mutableMapOf<Int, Array<Array<Operator>>>()
+    return equations
+        .filter { equation ->
+            val operatorsGroupSize = equation.numbers.size - 1
+            val operatorsGroup = operatorsGroupCache.getOrPut(operatorsGroupSize)
+            { crossProduct(*operators, power = operatorsGroupSize) }
+            operatorsGroup.any { equation.testEvaluation(*it) }
+        }
+        .sumOf { it.testValue }
+}
+
+private fun crossProduct(vararg operators: Operator, power: Int): Array<Array<Operator>> {
+    if (power < 0) {
+        return emptyArray()
+    }
+    return (0..<power)
+        .fold(arrayOf(emptyArray<Operator>()))
+        { product, index -> product.flatMap { el -> operators.map { t -> el + t } }.toTypedArray() }
 }
